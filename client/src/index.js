37088ae2e39bcd5060c4392f9d53fe0b094be535
trint <nttri238@gmail.com>
2023-10-24 17:03:18 +0700

    Client: responsive practice (cont)

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import App1 from './App1';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
