c7ed3c5d9efeb167e86e0e55ef6bc87e13968ec9
Tri Nguyen <trint@basebs.com>
2024-03-23 18:05:55 +0700

    MyConnect: pnpm build

--- MyConnect/client/vite.config.js
+++ MyConnect/client/vite.config.js
@@ -20,6 +20,6 @@ export default defineConfig({
     // "process.env": process.env,
     // // By default, Vite doesn't include shims for NodeJS/
     // // necessary for segment analytics lib to work
-    global: {},
+    _global: {},
   },
 });
